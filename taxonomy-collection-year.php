8f741cf55e6418fca5c942289f44da4c60b3b641
neligajb <boone@cobblehilldigital.com>
2016-09-08 09:09:40 -0400

    fixed collection template

--- taxonomy-collection-year.php
+++ taxonomy-collection-year.php
@@ -10,8 +10,7 @@ get_header(); ?>
 <header class="page-header collection-header">
   <!-- <img class="ornament" src="<?php echo get_template_directory_uri().'/assets/media/ornament-top.png' ?>" /> -->
   <h2 class="collections-heading"><?php echo $wp_query->queried_object->name; ?></h2>
-  <p><?php $categorydesc = category_description();
-        if ( ! empty( $categorydesc ) )  echo apply_filters( 'archive_meta', $categorydesc ); ?></p>
+
 </header>
 
 <div class="page-content">
